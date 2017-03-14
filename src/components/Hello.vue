eea90ee32190e98dcd78b9c63c9ce70f7c79c5ec
lizijian365 <lizijian365@163.com>
2017-03-14 22:41:57 +0800

    hello vue

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,53 +1,62 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    <h1 v-text="title"></h1>
+    <ul v-for="item in items">
+      <li :class="{finish: item.isFinished}" @click="toggleClick(item)">
+        {{item.label}}
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  export default {
+    name: 'TodoList',
+    data () {
+      return {
+        title: 'this is a todo list',
+        items: [
+          {
+            label: 'coding',
+            isFinished: false
+          },
+          {
+            label: 'walking',
+            isFinished: true
+          }
+        ]
+      }
+    },
+    methods: {
+      toggleClick: function (item) {
+        item.isFinished = !item.isFinished
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  .finish {
+    text-decoration: underline;
+  }
 
-a {
-  color: #42b983;
-}
+  li {
+    display: inline-block;
+    cursor:pointer;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
 </style>
