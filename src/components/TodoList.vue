22ee2794c23f20eeda4674fa1897cddc34fc2ecd
maxipeng <Mxp#1989#mxp>
2017-03-16 16:01:06 +0800

    登录页面

---
+++ src/components/TodoList.vue
@@ -0,0 +1,62 @@
+<template>
+  <el-row class="content">
+    <el-col :xs="{span:20,offset:2}" :sm="{span:8,offset:8}">
+      <span>
+        欢迎：{{name}}！你的待办事项是：
+      </span>
+      <el-input placeholder="请输入待办事项" v-model="todos" @keyup.enter.native="addTodos"></el-input>
+      <el-tabs v-model="activeName">
+
+        <el-tab-pane label="待办事项" name="first">
+          <el-col :xs="24">
+            <template v-if="!Done"> <!--v-if和v-for不能同时在一个元素内使用，因为Vue总会先执行v-for-->
+              <template v-for="(item, index) in list">
+                <div class="todo-list" v-if="item.status == false">
+                  <span class="item">
+                    {{ index + 1 }}. {{ item.content }}
+                  </span>
+                  <span class="pull-right">
+                    <el-button size="small" type="primary" @click="finished(index)">完成</el-button>
+                    <el-button size="small" :plain="true" type="danger" @click="remove(index)">删除</el-button>
+                  </span>
+                </div>
+              </template>
+            </template>
+            <div v-else-if="Done">
+              暂无待办事项
+            </div>
+          </el-col>
+        </el-tab-pane>
+
+        <el-tab-pane label="已完成事项" name="second">
+          <template v-if="count > 0">
+            <template v-for="(item, index) in list">
+              <div class="todo-list" v-if="item.status == true">
+                <span class="item finished">
+                  {{ index + 1 }}. {{ item.content }}
+                </span>
+                <span class="pull-right">
+                  <el-button size="small" type="primary" @click="restore(index)">还原</el-button>
+                </span>
+              </div>
+            </template>
+          </template>
+          <div v-else>
+            暂无已完成事项
+          </div>
+        </el-tab-pane>
+
+      </el-tabs>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+  export default{
+      data () {
+          return{
+              name:"Molunerfinn"
+          }
+      }
+  }
+</script>
