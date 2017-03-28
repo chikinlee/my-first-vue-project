0a5087e846274dd4d166587481c4020e630457a3
chikinlee <lizijian365@163.com>
2017-03-28 13:57:39 +0800

    提交

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-'use strict'
-// const app = require('koa')();
-const appKoa=require('koa');
-const koa = require('koa-router');
-const json = require('koa-json');
-const logger = require('koa-logger');
-const auth = require('./server/routes/auth');
-const koaBodyparser=require('koa-bodyparser');// 引入各种依赖
+const app = require('koa')()
+  , koa = require('koa-router')()
+  , json = require('koa-json')
+  , logger = require('koa-logger')
+  , auth = require('./server/routes/auth.js')
+  , api = require('./server/routes/api.js')
+  , jwt = require('koa-jwt')
+  , path =require('path')
+  , serve = require('koa-static')
+  , historyApiFallback = require('koa-history-api-fallback');
 
-const app = new appKoa();
-
-// app.use(koaBodyparser());
+app.use(require('koa-bodyparser')());
 app.use(json());
 app.use(logger());
 
@@ -17,16 +17,37 @@ app.use(function* (next){
   let start = new Date;
   yield next;
   let ms = new Date - start;
-  console.log('%s %s - %s', this.method, this.url, ms); // 显示执行的时间
+  console.log('%s %s - %s', this.method, this.url, ms);
+});
+
+app.use(function *(next){  //  如果JWT验证失败，返回验证失败信息
+  try {
+    yield next;
+  } catch (err) {
+    if (401 == err.status) {
+      this.status = 401;
+      this.body = {
+        success: false,
+        token: null,
+        info: 'Protected resource, use Authorization header to get access'
+      };
+    } else {
+      throw err;
+    }
+  }
 });
 
 app.on('error', function(err, ctx){
   console.log('server error', err);
 });
 
-koa.use('/auth',auth.routes())
 
-app.use(koa.routes())
+koa.use('/auth', auth.routes()); // 挂载到koa-router上，同时会让所有的auth的请求路径前面加上'/auth'的请求路径。
+koa.use("/api",jwt({secret: 'vue-koa-demo'}),api.routes()) // 所有走/api/打头的请求都需要经过jwt验证。
+
+app.use(koa.routes()); // 将路由规则挂载到Koa上。
+app.use(historyApiFallback());
+app.use(serve(path.resolve('dist'))); // 将webpack打包好的项目目录作为Koa静态文件服务的目录
 
 app.listen(8889,() => {
   console.log('Koa is listening in 8889');
